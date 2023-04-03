a16a74d62dbf82e4e1d0fdfde4d56b776f87d9d5
mesciuS <giovadev@gmail.com>
2023-04-03 16:48:25 +0200

    feat: flip card

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -35,17 +35,24 @@ export default {
 
 <template>
     <div id="movie-card">
-        <img :src="'https://image.tmdb.org/t/p/w342' + movie.poster_path" alt="">
+        <div id="movie-card-inner">
+            <div id="movie-card-front">
+                <img :src="'https://image.tmdb.org/t/p/w342' + movie.poster_path" alt="">
+            </div>
+            <div id="movie-card-back">
+                <ul>
+                    <li><em>Titolo:</em></li>
+                    <li><strong>{{ movie.title }}</strong></li>
+                    <li><em>Titolo originale:</em></li>
+                    <li><strong>{{ movie.original_title }}</strong></li>
+                    <li><em>Lingua originale:</em></li>
+                    <li><span :class="`fi fi-${flags()}`"></span></li>
+                    <li><em>Voto medio:</em></li>
+                    <li><strong>{{ movie.vote_average }}</strong></li>
+                </ul> 
+            </div>
+        </div>
     </div>
-        <br>
-        <em>Titolo:</em> <strong>{{ movie.title }}</strong>
-        <br>
-        <em>Titolo originale:</em> <strong>{{ movie.original_title }}</strong>
-        <br>
-        <em>Lingua originale:</em> <span :class="`fi fi-${flags()}`"></span>
-        <br>
-        <em>Voto medio:</em> <strong>{{ movie.vote_average }}</strong>
-        <hr>
 </template>
 
 <style lang="scss" scoped>
@@ -53,6 +60,36 @@ export default {
     width: 342px;
     height: 513px;
     border: 2px solid white;
+        #movie-card {
+            &:hover{
+                transform: rotateY(180deg);
+            }
+        }
+        #movie-card-inner{
+            position: relative;
+            width: 100%;
+            height: 100%;
+            text-align: center;
+            transition: transform 1s;
+            transform-style: preserve-3d;
+            &:hover {
+            transform: rotateY(180deg);
+            }
+        }   #movie-card-front, #movie-card-back {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            backface-visibility: hidden;
+        }
+        #movie-card-back {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #141414;
+            color: white;
+            transform: rotateY(180deg);
+        }
+
    }
    img {
     width: 100%;
--- src/components/TVitem.vue
+++ src/components/TVitem.vue
@@ -35,14 +35,21 @@ export default {
 
 <template>
     <div id="tv-card">
-        <img :src="'https://image.tmdb.org/t/p/w342' + show.poster_path" alt="">
-        <br>
-        <em>Titolo:</em> <strong>{{ show.original_name }}</strong>
-        <br>
-        <em>Lingua originale:</em> <span :class="`fi fi-${TVflags()}`"></span>
-        <br>
-        <em>Voto medio:</em> <strong>{{ show.vote_average }}</strong>
-        <hr>
+        <div id="tv-card-inner">
+            <div id="tv-card-front">
+                <img :src="'https://image.tmdb.org/t/p/w342' + show.poster_path" alt="">
+            </div>
+            <div id="tv-card-back">
+                <ul>
+                    <li><em>Titolo:</em></li>
+                    <li><strong>{{ show.original_name }}</strong></li>
+                    <li><em>Lingua originale:</em></li>
+                    <li><span :class="`fi fi-${TVflags()}`"></span></li>
+                    <li><em>Voto medio:</em></li>
+                    <li><strong>{{ show.vote_average }}</strong></li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -51,6 +58,36 @@ export default {
     width: 342px;
     height: 513px;
     border: 2px solid white;
+        #tv-card {
+            &:hover{
+                transform: rotateY(180deg);
+            }
+        }
+        #tv-card-inner{
+            position: relative;
+            width: 100%;
+            height: 100%;
+            text-align: center;
+            transition: transform 1s;
+            transform-style: preserve-3d;
+            &:hover {
+            transform: rotateY(180deg);
+            }
+        }   #tv-card-front, #tv-card-back {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            backface-visibility: hidden;
+        }
+        #tv-card-back {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #141414;
+            color: white;
+            transform: rotateY(180deg);
+        }
+
    }
    img {
     width: 100%;
